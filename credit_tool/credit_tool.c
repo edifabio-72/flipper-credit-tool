a7d7673c9bdb64569a9f0078ba3a281af2707204
Ettore <75947873+edifabio-72@users.noreply.github.com>
2025-07-14 10:39:05 +0200

    Create credit_tool.c
    # Codice sorgente principale

---
+++ credit_tool/credit_tool.c
@@ -0,0 +1,8 @@
+#include <furi.h>
+
+// Punto di ingresso principale per l'app Flipper Zero
+int32_t credit_tool_app(void* p) {
+    FURI_LOG_I("CreditTool", "App avviata!");
+    // Qui andrà il tuo codice principale
+    return 0;
+}
